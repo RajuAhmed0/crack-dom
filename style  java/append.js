7fc1518453086ef4706f399d87936c914c6feabd
Your Name <you@example.com>
2024-08-14 00:48:26 +0600

    added

---
+++ style  java/append.js
@@ -0,0 +1,11 @@
+// New element added way
+const added = document.getElementById('added-li'); /* access the id */
+
+const li = document.createElement('li'); /* creat the tag */
+li.innerText = 'WaterMalon';
+
+// added.appendChild(li); /* linkup the tag from id */
+
+
+
+
